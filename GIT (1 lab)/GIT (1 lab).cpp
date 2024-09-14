d8167e371de1eca25f92d5f2db5dd78ce542e2a0
Bogdanov Ilya PI-32 <Saifect@mail.ru>
2024-09-14 21:20:20 +0700

    Resolved merge conflict by keeping the correct version

--- GIT (1 lab)/GIT (1 lab).cpp
+++ GIT (1 lab)/GIT (1 lab).cpp
@@ -1,24 +1,12 @@
-<<<<<<< Updated upstream
-﻿// GIT (1 lab).cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+// GIT (1 lab).cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
 #include <stdio.h>
 
 int main()
 {
-<<<<<<< HEAD
-    std::cout << "Hello World!\n";
-=======
-﻿
-#include <stdio.h>
-
-int main()
-{
-   
->>>>>>> Stashed changes
-=======
     int num;
     scanf("%d", &num);
     printf("%d", num);
->>>>>>> 0099a1f3e585dcfff7c0e20d47356fb04dacfb57
 }
+
